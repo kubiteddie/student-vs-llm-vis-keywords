fbbde1cf612411f75fe482122e406bb09897d02a
unknown <eddiekubit0@gmail.com>
2024-04-07 22:54:54 -0400

    switched to cohere query

--- llm_keyword_gen.py
+++ llm_keyword_gen.py
@@ -3,12 +3,21 @@ import spacy
 import cohere
 import google.generativeai as genai
 
-classdata = pd.read_csv("datastore/class-data-collection.csv")
-GOOGLE_API_KEY = open('gemini-api-key.txt')
-genai.configure(api_key=GOOGLE_API_KEY)
+def gemini_init():
+    GOOGLE_API_KEY = open('gemini-api-key.txt')
+    genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("Will I be able to talk to you for free or will my google account be charged")
-print(response)
-print(response.text)
+    return model
+
+def cohere_init():
+    co = cohere.Client("luN4E3VyjZumWO17DsMUN43ZrFDuS81rHuPvrEk5")
+    return co
+
+def get_keywords(co, dataSubset):
+   pass 
+
+if __name__ == '__main__':
+    co = cohere_init()
+    classdata = pd.read_csv("datastore/class-data-collection.csv")
