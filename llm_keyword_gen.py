84ac9f970c0866ec61351be872566d96612a1027
unknown <eddiekubit0@gmail.com>
2024-04-07 21:17:31 -0400

    first test of gemini interaction

--- llm_keyword_gen.py
+++ llm_keyword_gen.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import spacy
+import cohere
+import google.generativeai as genai
 
 classdata = pd.read_csv("datastore/class-data-collection.csv")
+GOOGLE_API_KEY = open('gemini-api-key.txt')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+response = model.generate_content("Will I be able to talk to you for free or will my google account be charged")
+print(response)
+print(response.text)
